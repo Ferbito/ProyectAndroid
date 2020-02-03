dabf2bbfcad455e73e54145590b174733d4f4bea
rub19 <rub1999g@gmail.com>
2020-02-03 18:58:15 +0100

    enlace url api games

--- app/src/main/java/com/example/gamesclub/MainActivity.java
+++ app/src/main/java/com/example/gamesclub/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //AQUI EMPIEZA TODO
+       // https://api.rawg.io/api/games?
     }
 }
