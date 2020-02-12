ea56b287a652f18b82c2887176e22b404e9049d4
rub19 <rub1999g@gmail.com>
2020-02-12 20:11:45 +0100

    la pagina de inicio slider

--- app/src/main/java/com/example/gamesclub/MainActivity.java
+++ app/src/main/java/com/example/gamesclub/MainActivity.java
@@ -18,7 +18,7 @@ private TextView[] mDots;
 private Button mButonGame;
 private Button mButonBuscar;
 private Typeface script;
-private String []imageUrls=new String[]{"https://img.icons8.com/bubbles/2x/iron-man.png","https://synth.agency/wp-content/uploads/2019/07/Marvel-Hulk-1024x819.png","https://cdn4.iconfinder.com/data/icons/superhero-3/500/Superhero-01-512.png" };
+private String []imageUrls=new String[]{"https://img.icons8.com/bubbles/2x/iron-man.png","https://synth.agency/wp-content/uploads/2019/07/Marvel-Hulk-1024x819.png","https://cdn4.iconfinder.com/data/icons/superhero-3/500/Superhero-01-512.png","https://icons-for-free.com/iconfiles/png/512/super+thor+wings+icon-1320166699905266736.png" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +39,9 @@ private String []imageUrls=new String[]{"https://img.icons8.com/bubbles/2x/iron-
     }
     public void addDotsIndicator(int position){
 
-        mDots=new TextView[3];
+        mDots=new TextView[5];
         mDotLayout.removeAllViews();
+
         for (int i=0;i<mDots.length;i++){
             mDots[i]=new TextView(this);
             mDots[i].setText(Html.fromHtml("&#8226;"));
@@ -48,6 +49,7 @@ private String []imageUrls=new String[]{"https://img.icons8.com/bubbles/2x/iron-
             mDots[i].setTextColor(getResources().getColor(R.color.colorTransparentWhite));
             mDots[i].setTypeface(script);
             mDotLayout.addView(mDots[i]);
+
         }
         if(mDots.length>0){
             mDots[position].setTextColor(getResources().getColor(R.color.red));
