25fa2804e398be79375926446bf8755da5a47b4e
Ferbito <fmore.g@gmail.com>
2020-03-06 20:46:43 +0100

    Perfeccion

--- app/src/main/java/com/example/comicsclub/FavoritosTiendas.java
+++ app/src/main/java/com/example/comicsclub/FavoritosTiendas.java
@@ -1,18 +1,23 @@
 package com.example.comicsclub;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.gson.Gson;
@@ -21,26 +26,34 @@ import com.squareup.picasso.Picasso;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class FavoritosTiendas extends AppCompatActivity {
     private List<TiendasResponse.Tiendas> mTiendasFavorito=new ArrayList<>();
     private ListView mLv = null;
-    //private FavoritosTiendas.MyAdapter mAdapter;
+    private MyAdapter mAdapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favoritos_tiendas);
-
+        Toast.makeText(FavoritosTiendas.this, "WELCOME TO FAVORITES", Toast.LENGTH_SHORT).show();
 
 
         mLv = findViewById(R.id.list_fav);
         leerDatosSP();
 
-        Toast.makeText(FavoritosTiendas.this, "WELCOME TO FAVORITES", Toast.LENGTH_SHORT).show();
-
+        mLv.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
+            @Override
+            public void onCreateContextMenu(ContextMenu contextMenu,
+                                            View view,
+                                            ContextMenu.ContextMenuInfo contextMenuInfo) {
+                contextMenu.add(0, 1, 0, "ELIMINAR");
+            }
+        });
     }
 
     private void guardarDatoSP(){
@@ -61,22 +74,21 @@ public class FavoritosTiendas extends AppCompatActivity {
         Log.d("PERSIST", String.valueOf(restoreArray.size()));
         if(restoreArray!=null){
             mTiendasFavorito=restoreArray;
+            Collections.sort(mTiendasFavorito, new Comparator<TiendasResponse.Tiendas>(){
+                public int compare(TiendasResponse.Tiendas obj1,
+                                   TiendasResponse.Tiendas obj2) {
+                    return obj1.getDistance().compareTo(obj2.getDistance());
+                }
+            });
             for (int i =0; i<mTiendasFavorito.size(); i++) {
                 Log.d("PERSIST2", mTiendasFavorito.get(i).getName());
             }
-            MyAdapter mAdapter = new MyAdapter();
+            mAdapter = new MyAdapter();
             mLv.setAdapter(mAdapter);
         }
     }
 
     private class MyAdapter extends BaseAdapter {
-
-       /* private Context mContext;
-
-
-        public MyAdapter(Context context) {
-            this.mContext = context;
-        }*/
 
         @Override
         public int getCount() {
@@ -117,5 +129,19 @@ public class FavoritosTiendas extends AppCompatActivity {
 
             return myview;
         }
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info=(AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case 1:
+                mTiendasFavorito.remove(info.position);
+                Toast.makeText(FavoritosTiendas.this,"ELIMINADO DE FAVORITOS", Toast.LENGTH_SHORT).show();
+                guardarDatoSP();
+                mAdapter.notifyDataSetChanged();
+                break;
+        }
+        return true;
     }
 }
